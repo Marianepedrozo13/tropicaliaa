3b43463f6bfb0e9228f7c43c6d12f516fc00f7ef
Marianepedrozo13 <151068207+Marianepedrozo13@users.noreply.github.com>
2025-03-25 13:44:11 -0300

    ...

--- script.js
+++ script.js
@@ -21,4 +21,7 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 botaoDeAcessibilidade.addEventListener('click', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+})const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste')
 })
